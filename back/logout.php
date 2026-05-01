658fa92669e65ecee7cb1ed1028a9fad81703e86
emna ghourabi <emna.ghourabi@isimg.tn>
2026-05-01 23:32:31 +0100

    Update logout.php

--- back/logout.php
+++ back/logout.php
@@ -4,8 +4,3 @@ session_destroy(); // Détruit la session côté serveur
 header('Location: /gharbouch/html/connexion.php'); // Redirige vers connexion
 exit;
 ?>
-<script>
-  localStorage.removeItem('gharbouch_user'); // ← يمسح من localStorage
-  localStorage.removeItem('gharbouch_cart');
-  window.location.href = '/gharbouch/html/connexion.php';
-</script>
